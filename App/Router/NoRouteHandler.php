97c5d0b43c5d0802a76330816276d6a726531868
Randy Esplin <randy.esplin@gmail.com>
2026-06-12 10:09:28 -0700

    Drop ObjectManager

--- App/Router/NoRouteHandler.php
+++ App/Router/NoRouteHandler.php
@@ -9,7 +9,6 @@ namespace ECInternet\CatalogFeatures\App\Router;
 
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\App\RequestInterface;
-use Magento\Framework\App\Router\NoRouteHandlerInterface;
 use Magento\Framework\Filesystem\Io\File;
 use Magento\Framework\Http\PhpEnvironment\Request as HttpRequest;
 use ECInternet\CatalogFeatures\Helper\Data;
